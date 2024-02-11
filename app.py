5758bcf461d0d695e6771ed90cc4af76de799219
lejames <okejames55@gmail.com>
2024-02-11 14:38:17 +0100

    Send the user location to Flask backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
 from utility.helpers import read_gtfs_data
 
@@ -11,6 +11,12 @@ stops_geojson = stops_gdf.to_json()
 @app.route('/')
 def index():
     return render_template('index.html', stops_geojson=stops_geojson)
+
+@app.route('/process_user_position', methods=['POST'])
+def process_user_position():
+    user_position = request.get_json()
+
+    return jsonify({'message': 'User position received and processed successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
